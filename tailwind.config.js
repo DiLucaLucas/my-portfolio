6c6af3dee8e50074b54127bb287a91ce01c1c586
Lucas Di Luca <84538567+LucasDiLuca26@users.noreply.github.com>
2024-06-07 16:55:03 -0300

    Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue": "#222438",
+        "customHover": "#F04A52",
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
+  variants: {},
   plugins: [],
 };
